b434d2ca857786ee4a1220865b17a130ff32987a
Vova2999 <Vova2999@gmail.com>
2018-01-24 21:19:28 +0400

    Создал проект

---
+++ WpfApplicationPatcher.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfApplicationPatcher.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfApplicationPatcher.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3772aca-b801-4f80-92a3-0a605fdc1763")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfApplicationPatcher/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace WpfApplicationPatcher {
+	public static class Program {
+		public static void Main(string[] args) {
+		}
+	}
+}
